2b7e09737a7fbf9c41869d8edbef49ff85ca7a97
Omas Ajiri <jicintech@gmail.com>
2021-01-14 11:17:05 +0100

    feat: added an edit page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path')
 const app = express()
 const VitaModel = require('./models/model')
 const ejsMate = require('ejs-mate')
+const methodOverride = require('method-override')
 
 const options = {
     useNewUrlParser: true,
@@ -22,7 +23,7 @@ app.set('views',  path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-
+app.use(methodOverride('_method'))
 
 app.get('/vitamins', async (req, res) => {
     const vitamins = await VitaModel.find()
@@ -43,6 +44,17 @@ app.post('/vitamins/create', async (req, res) => {
     await newVit.save()
     res.redirect(`/vitamins/${newVit.id}`)
 
+})
+app.get('/vitamins/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const vitamin = await VitaModel.findById(id)
+    res.render('vitamin/edit', {vitamin})
+})
+app.put('/vitamins/:id', async (req, res) => {
+    const { id } = req.params
+    const body = req.body;
+    const vitamin = await VitaModel.findByIdAndUpdate(id, body, {runValidators: true, new: true})
+    res.redirect(`/vitamins/${vitamin.id}`)
 })
 app.listen(port, () => {
     console.log(`serving the from port ${port}`)
