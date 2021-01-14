2f9155f543be08669a3edd12b05eeec2f0bcd3fb
Omas Ajiri <jicintech@gmail.com>
2021-01-14 10:54:18 +0100

    feat: added new vitamin page

--- app.js
+++ app.js
@@ -20,17 +20,29 @@ mongoose.connect(uri, options)
 app.engine('ejs', ejsMate)
 app.set('views',  path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
 
 app.get('/vitamins', async (req, res) => {
     const vitamins = await VitaModel.find()
     res.render('vitamin/index', {vitamins})
 })
+app.get('/vitamins/new', (req, res) => {
+    res.render('vitamin/new')
+})
 
 app.get('/vitamins/:id', async (req, res) => {
     const { id } = req.params
     const vitamin = await VitaModel.findById(id)
     res.render('vitamin/show', { vitamin })
+})
+app.post('/vitamins/create', async (req, res) => {
+    const body = req.body;
+    const newVit = new VitaModel(body)
+    await newVit.save()
+    res.redirect(`/vitamins/${newVit.id}`)
+
 })
 app.listen(port, () => {
     console.log(`serving the from port ${port}`)
